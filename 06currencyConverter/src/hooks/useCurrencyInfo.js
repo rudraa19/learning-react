1e261c091577d282ab44453348ee2ed7c4498f92
rudraa19 <rudra19206@gmail.com>
2024-05-23 23:31:33 +0530

    updated

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react"
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://open.er-api.com/v6/latest/${String(currency).toUpperCase()}`)
+        fetch(`https://open.er-api.com/v6/latest/${currency}`)
         .then((res) => res.json())
         .then((res) => setData(res["rates"]))
     }, [currency])
